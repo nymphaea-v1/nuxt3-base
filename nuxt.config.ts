656a76c35a0f3b98e96fc9616d9d0c25aa754a4c
nymphaea-v1 <roselin.6@yandex.ru>
2023-07-13 13:50:09 +0500

    add head property to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,15 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      // title:  'title',
-      // meta: [
-      //   { name: 'description', content: 'description' }
-      // ]
+      title: 'title',
+      meta: [
+        { name: 'description', content: 'description' }
+      ],
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover',
+      htmlAttrs: { lang: 'en' },
+      // All css classes applied to child components should be prefixed with #override selector
+      // https://github.com/vuejs/vue-loader/issues/808, https://github.com/nuxt/nuxt/issues/15048
+      bodyAttrs: { id: 'override' }
     }
-  },
-  css: ['assets/styles/global.css'],
-  ssr: true
+  }
 })
